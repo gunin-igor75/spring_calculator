345904eb5527921c85671f32f2cf98e57790e178
Igor Gunin <gunin-igor75@yandex.ru>
2022-09-08 09:50:51 +0600

    Fix 2.0 add check

--- src/main/java/pro/sky/spring_calculator/Calculator.java
+++ src/main/java/pro/sky/spring_calculator/Calculator.java
@@ -2,6 +2,8 @@ package pro.sky.spring_calculator;
 
 import org.springframework.stereotype.Service;
 
+import javax.print.DocFlavor;
+
 @Service
 public class Calculator implements CalculatorService {
     @Override
@@ -11,38 +13,30 @@ public class Calculator implements CalculatorService {
 
     @Override
     public String plus(Integer num1, Integer num2) {
-        if (num1 == null && num2 == null) {
-            return "<h1>Не переданы параметры<h2/>";
-        } else if (num1 == null) {
-            return "<h2>Не передан параметр num2<h2/>";
-        } else if (num2 == null) {
-            return "<h2>Не передан параметр num1<h2/>";
+        String response = check(num1, num2);
+        if (!response.equals("good")) {
+            return response;
         }
         int suma = num1 + num2;
         return String.format("<h2>%d + %d = %d<h2/>", num1, num2, suma);
     }
 
+
     @Override
     public String minus(Integer num1, Integer num2) {
-        if (num1 == null && num2 == null) {
-            return "<h1>Не переданы параметры<h2/>";
-        } else if (num1 == null) {
-            return "<h2>Не передан параметр num2<h2/>";
-        } else if (num2 == null) {
-            return "<h2>Не передан параметр num1<h2/>";
+        String response = check(num1, num2);
+        if (!response.equals("good")) {
+            return response;
         }
-            int delta = num1 - num2;
+        int delta = num1 - num2;
         return String.format("<h2>%d - %d = %d<h2/>", num1, num2, delta);
     }
 
     @Override
     public String multiply(Integer num1, Integer num2) {
-        if (num1 == null && num2 == null) {
-            return "<h1>Не переданы параметры<h2/>";
-        } else if (num1 == null) {
-            return "<h2>Не передан параметр num2<h2/>";
-        } else if (num2 == null) {
-            return "<h2>Не передан параметр num1<h2/>";
+        String response = check(num1, num2);
+        if (!response.equals("good")) {
+            return response;
         }
         int multiplication = num1 * num2;
         return String.format("<h2>%d * %d = %d<h2/>", num1, num2, multiplication);
@@ -50,16 +44,26 @@ public class Calculator implements CalculatorService {
 
     @Override
     public String divide(Integer num1, Integer num2) {
-        if (num1 == null && num2 == null) {
-            return "<h2>Не переданы параметры<h2/>";
-        } else if (num1 == null) {
-            return "<h2>Не передан параметр num1<h2/>";
-        } else if (num2 == null) {
-            return "<h2>Не передан параметр num2<h2/>";
-        } else if (num2 == 0) {
+        String response = check(num1, num2);
+        if (!response.equals("good")) {
+            return response;
+        }
+        if (num2 == 0) {
             return "<h1>Деление на 0<h2>";
         }
         int divider = num1 / num2;
         return String.format("<h2>%d / %d = %d<h2/>", num1, num2, divider);
+    }
+
+    private String check(Integer num1, Integer num2) {
+        String response = "good";
+        if (num1 == null && num2 == null) {
+            response = "<h2>Не переданы параметры<h2/>";
+        } else if (num1 == null) {
+            response = "<h2>Не передан параметр num1<h2/>";
+        } else if (num2 == null) {
+            response = "<h2>Не передан параметр num2<h2/>";
+        }
+        return response;
     }
 }
